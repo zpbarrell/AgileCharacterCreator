8a7fa85eb38bdbd81e1d7f7bf1b9976506962526
ncaldwell0922 <104471006+ncaldwell0922@users.noreply.github.com>
2022-08-18 19:07:56 -0400

    Revert "Nc player atts"

--- AgileCharacterCreator.WebAPI/Program.cs
+++ AgileCharacterCreator.WebAPI/Program.cs
@@ -1,20 +1,11 @@
-using AgileCharacterCreator.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
- {
-     c.SwaggerDoc("v1", new OpenApiInfo { Title = "ElevenNote.WebAPI", Version = "v1" });
-     });
-     
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
