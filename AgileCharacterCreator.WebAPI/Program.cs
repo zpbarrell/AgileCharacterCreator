b901ca05730bd90cf0a69ae4157381245ed06af8
Zack Barrell <zpbarrell@gmail.com>
2022-08-18 18:47:04 -0400

    DBcontext added

--- AgileCharacterCreator.WebAPI/Program.cs
+++ AgileCharacterCreator.WebAPI/Program.cs
@@ -1,11 +1,20 @@
+using AgileCharacterCreator.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+ {
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "ElevenNote.WebAPI", Version = "v1" });
+     });
+     
 
 var app = builder.Build();
 
